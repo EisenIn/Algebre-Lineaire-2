6dc9854f45e989817de4150e72684b64ef1f1cef
Friedrich Eisenbrand <fritz.eisenbrand@gmail.com>
2024-02-27 10:56:26 +0100

    Created subfolders and moved files

---
+++ Programming/C++/CMakeFiles/Test.o.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/./Test.o.dir/Test.cpp.o"
+  "./Test.o.pdb"
+  "./Test.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/./Test.o.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Programming/C++/CMakeFiles/Test.o.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Test.o.
+# This may be replaced when dependencies are built.
